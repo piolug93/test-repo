8ceecf3d6843883b71f040a00e3028743be501c3
piolug93 <piolug93@gmail.com>
2017-12-23 18:22:50 +0100

    Add SendUSSD function
    Refactor StateLTE.
    Change curl commands to variables so as eliminate print them to screen.
    Add return result with GetUSSD.

--- LTE.sh
+++ LTE.sh
@@ -2,7 +2,7 @@
 function token {
 	DATA=`curl http://192.168.8.1/api/webserver/SesTokInfo`
 	SID=`echo "$DATA" | grep "SessionID=" | cut -b 10-147`
-	TOKEN=`echo "$DATA" | grep "TokInfo" | cut -b 10-41` 
+	TOKEN=`echo "$DATA" | grep "TokInfo" | cut -b 10-41`
 }
 
 function ChangeNetworkMode {
@@ -10,52 +10,61 @@ function ChangeNetworkMode {
 	case $1 in
 		LTE)
 		tryb="03"
-		echo "$tryb"
 		;;
 		LTE_UMTS)
 		tryb="0302"
 		;;
 	esac
 	token
-	curl http://192.168.8.1/api/net/net-mode -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><NetworkMode>'$tryb'</NetworkMode><NetworkBand>3FFFFFFF</NetworkBand><LTEBand>7FFFFFFFFFFFFFFF</LTEBand></request>' 
+	T=`curl http://192.168.8.1/api/net/net-mode -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><NetworkMode>'$tryb'</NetworkMode><NetworkBand>3FFFFFFF</NetworkBand><LTEBand>7FFFFFFFFFFFFFFF</LTEBand></request>'`
 }
 
 function SwitchData {
 	token
-	curl http://192.168.8.1/api/dialup/mobile-dataswitch -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><dataswitch>'$1'</dataswitch></request>' 
+	T=`curl http://192.168.8.1/api/dialup/mobile-dataswitch -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><dataswitch>'$1'</dataswitch></request>'`
 }
 
 function SendSMS {
 	token
 	curl http://192.168.8.1/api/sms/send-sms -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><Index>-1</Index><Phones><Phone>'$1'</Phone></Phones><Sca></Sca><Content>'$2'</Content><Length>4</Length><Reserved>1</Reserved><Date>2017-12-22 17:01:49</Date></request>'
 }
-# dopisać zmianę na UMTS
+
 function StateLTE {
-	token
-	curl http://192.168.8.1/api/ussd/send -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<request><content>*111*480*3#</content><codeType></codeType><timeout>5</timeout></request>'
+	SendUSSD "*111*480*3#"
 	sleep 5
-	token
-	respond=`curl http://192.168.8.1/api/ussd/get -b "$SID" -H "__RequestVerificationToken: $TOKEN" | grep "content" | sed -e 's/<[^>]*>//g' | head -n1`
-	if [ "$respond" == "Usluga wlaczona" ]
+	respond=`GetUSSD | grep "content" | sed -e 's/<[^>]*>//g' | head -n1 | cut -c 3-18` 
+	echo "$respond"
+	if [ "$respond" == 'Usluga wlaczona ' ]
 		then
 		echo "Wlaczona OK"
 	elif [ "$respond" == "Usluga wylaczona" ]
 		then
-		echo "Wylczona, zle"
+		echo "Wylaczona, zle"
 		#SendSMS "nr_tel" "tresc"
 	else
-		echo 'Blad: ' $wynik
+		echo 'Blad'
 	fi
+}
+
+function GetUSSD {
+	SwitchData "0"
+	ChangeNetworkMode "LTE_UMTS"
+	sleep 2
+	token
+	get=`curl http://192.168.8.1/api/ussd/get -b "$SID" -H "__RequestVerificationToken: $TOKEN"`
+	ChangeNetworkMode "LTE"
+	SwitchData "1"
+	echo $get
 }
 
 function SendUSSD {
 	SwitchData "0"
 	ChangeNetworkMode "LTE_UMTS"
 	token
-	curl http://192.168.8.1/api/ussd/send -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<request><content>'$1'</content><codeType></codeType><timeout>5</timeout></request>'
-	sleep 5
-	ChangeNetworkMode "LTE"
-	SwitchData "1"
+	sleep 2
+	T=`curl http://192.168.8.1/api/ussd/send -b "$SID" -H "__RequestVerificationToken: $TOKEN" --data-binary $'<request><content>'$1'</content><codeType></codeType><timeout>5</timeout></request>'`
+	#ChangeNetworkMode "LTE"
+	#SwitchData "1"
 }
 
 function ActivateFreeLTE {
@@ -63,6 +72,5 @@ function ActivateFreeLTE {
 }
 
 
-ActivateFreeLTE
-sleep 5
+#ActivateFreeLTE
 StateLTE
